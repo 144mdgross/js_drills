9b7005627317399a6924a50ebef1f85c8d089676
144mdgross <144.mdgross@gmail.com>
2017-08-03 20:33:53 -0600

    adds test folder. up nest converting test to mocha/chai

---
+++ test/num_to_word_test.js
@@ -0,0 +1,19 @@
+// goal convert tests to mocha/chai 
+
+Test.assertEquals(number2words(0),"zero");
+Test.assertEquals(number2words(1),"one");
+Test.assertEquals(number2words(8),"eight");
+Test.assertEquals(number2words(10),"ten");
+Test.assertEquals(number2words(19),"nineteen");
+Test.assertEquals(number2words(20),"twenty");
+Test.assertEquals(number2words(22),"twenty-two");
+Test.assertEquals(number2words(54),"fifty-four");
+Test.assertEquals(number2words(80),"eighty");
+Test.assertEquals(number2words(98),"ninety-eight");
+Test.assertEquals(number2words(100),"one hundred");
+Test.assertEquals(number2words(301),"three hundred one");
+Test.assertEquals(number2words(793),"seven hundred ninety-three");
+Test.assertEquals(number2words(800),"eight hundred");
+Test.assertEquals(number2words(650),"six hundred fifty");
+Test.assertEquals(number2words(1000),"one thousand");
+Test.assertEquals(number2words(1003),"one thousand three");
