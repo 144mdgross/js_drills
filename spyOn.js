606421066b6568f8c1c28e484792e30b33ce3e8b
144mdgross <144.mdgross@gmail.com>
2017-10-16 17:20:12 -0600

    removes tests from solutions branch

--- spyOn.js
+++ spyOn.js
@@ -6,7 +6,7 @@
 // What contexts it was called in
 // What values it returned
 // Whether it threw an error
-// For this kata, implement a spyOn function which takes any function func as a parameter and returns a spy for func. The returned spy must be callable in the same manner as the original func, and include the following additional properties/methods:
+// Implement a spyOn function which takes any function as a parameter and returns a spyOn func. The returned spy must be callable in the same manner as the original func, and include the following additional properties/methods:
 
 // .callCount() — returns the number of times spy has been called
 // .wasCalledWith(val) – returns true if spy was ever called with val, else returns false.
